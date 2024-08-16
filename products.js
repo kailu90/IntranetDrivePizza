712034f21db6c2f3105d2fba47c3967862d0074c
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-08-16 11:50:38 -0500

    fix: corregido llamado a la API

--- products.js
+++ products.js
@@ -1,16 +1,11 @@
 async function callApi(URL) {
     try {
-      const respuesta = await fetch(URL) 
-      const datos = await respuesta.json()
-      console.log(datos) 
+      const respuesta = await fetch(URL);
+      const datos = await respuesta.json();
+      console.log(datos);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-async(callApi("https://api-pizzeria.vercel.app/api/v1/products"))()
-
-document.getElementById('login_form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita el envío del formulario por defecto
-  window.location.href = 'products.html'; // Redirige a index.html
-})
+callApi("https://api-pizzeria.vercel.app/api/v1/products");
