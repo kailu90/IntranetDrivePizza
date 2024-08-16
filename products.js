a71296320df1b85e20714f734f2a6006b6538e27
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-08-16 14:06:00 -0500

    feature: captura de datos del formulario y envío a la API

--- products.js
+++ products.js
@@ -9,3 +9,53 @@ async function callApi(URL) {
 }
 
 callApi("https://api-pizzeria.vercel.app/api/v1/products");
+
+const productForm = document.getElementById('form');
+
+productForm.addEventListener('submit', function (event) {
+ event.preventDefault();
+
+ const form = event.target;
+ const order = createOrder(form);
+ postData(order);
+});
+
+function createOrder(data) {
+  console.log(data.elements)
+
+  const newOrder = {};
+  
+  Array.from(data.elements).forEach(element => {
+    if (element.name) {
+      if (element.value !== "0") {
+       const key = element.name.split('_').join(' ');
+        newOrder[key] = element.value;
+      }
+    }
+  });
+  
+  console.log(newOrder)
+
+  return newOrder;
+}
+
+async function postData(orderData) {
+ try {
+  const respond = await fetch("https://api-pizzeria.vercel.app/api/v1/orders", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderData)
+  });
+  
+  if(!respond.ok) {
+    throw new Error(`error status: ${respond.status}`);
+  }
+
+  const data = await respond.json();
+  console.log(data);
+ } catch (error) {
+  console.error(error);
+ } 
+}
