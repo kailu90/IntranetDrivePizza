80852a2e766be1a272fdbd79ee14bbcdc8a0f886
kailu90 <czuluaga2018@gmail.com>
2024-08-30 21:40:31 -0500

    se modifica window.location por history.back, para editar pedido

--- orders.js
+++ orders.js
@@ -1,11 +1,11 @@
-document.getElementById('btn_edit').addEventListener('click', function() {
-    window.location.href = 'products.html';
-    console.log("botón editar")
-  });
-
+  document.getElementById('btn_edit').addEventListener('click', function(event) {
+  event.preventDefault(); // Evita el comportamiento predeterminado del botón
+  history.back(); // Regresa a la página anterior sin refrescar
+  console.log("botón editar");
+});
   
   document.getElementById('btn_confirm').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = 'envioOK.html';
-    console.log("botón confirmar pedido")
+  event.preventDefault();
+  window.location.href = 'envioOK.html';
+  console.log("botón confirmar pedido")
   });
