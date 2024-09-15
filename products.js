fb8d06bcdc809af7f1356c1ab775ad330c394633
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-09-14 21:47:32 -0500

    Refactor: cambio en la tabla de confirmar pedido para calculary mostrar los precios de los productos

--- products.js
+++ products.js
@@ -1,28 +1,28 @@
 const productForm = document.getElementById('form');
 
-productForm.addEventListener('submit', function (event) {
- event.preventDefault();
- 
- const form = event.target;
- saveOrder(form);
+productForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
 
- window.location.href = 'cart.html';
+  const form = event.target;
+  saveOrder(form);
+
+  window.location.href = 'cart.html';
 });
 
 function saveOrder(data) {
   const newOrder = {};
-  
+
   Array.from(data.elements).forEach(element => {
     if (element.name) {
       if (element.value !== "0" && element.value !== "") {
-       const key = element.name.split('_').join(' ');
+        const key = element.name.split('_').join(' ');
         newOrder[key] = element.value;
       }
     }
   });
-  
+
 
   if (Object.keys(newOrder).length !== 0) {
-    localStorage.setItem('order', JSON.stringify(newOrder));    
+    localStorage.setItem('order', JSON.stringify(newOrder));
   }
 }
