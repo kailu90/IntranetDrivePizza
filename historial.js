45b622c1725c08c68d76b84b0283e2ca6d9beffc
kailu90 <czuluaga2018@gmail.com>
2024-12-05 09:05:29 -0500

    Se adicionan cambios, se adiciona sede del unico

---
+++ historial.js
@@ -0,0 +1,9 @@
+document.getElementById("card2").addEventListener("click", function() {
+    window.location.href = "historial.html"; 
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let sede = "Acrópolis";
+    console.log("sede acrópolis");
+    document.title = `Historial Pedidos ${sede}`;
+});
